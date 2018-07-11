2a80a4de830586d8d6331ebbb8e9b8140ca2d8eb
René Schultz Madsen <rm@microting.com>
2018-07-11 10:46:14 +0200

    Adding the base project.

---
+++ Microting.WindowsService.BasePn/ISdkEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Microting.WindowsService.BasePn
+{
+    public interface ISdkEventHandler
+    {
+        void CoreEventException(object sender, EventArgs args);
+        void UnitActivated(object sender, EventArgs args);
+        void eFormProcessed(object sender, EventArgs args);
+        void eFormProcessingError(object sender, EventArgs args);
+        void eFormRetrived(object sender, EventArgs args);
+        void CaseCompleted(object sender, EventArgs args);
+        void CaseDeleted(object sender, EventArgs args);
+        void NotificationNotFound(object sender, EventArgs args);
+    }
+}
