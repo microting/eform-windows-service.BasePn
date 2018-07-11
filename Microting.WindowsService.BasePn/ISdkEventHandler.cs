3eb32071b9977c2e4dadc842a63b6ea85333ea20
René Schultz Madsen <rm@microting.com>
2018-07-11 15:44:47 +0200

    Updating the interface to include the start, stop and restart methods.

--- Microting.WindowsService.BasePn/ISdkEventHandler.cs
+++ Microting.WindowsService.BasePn/ISdkEventHandler.cs
@@ -12,5 +12,24 @@ namespace Microting.WindowsService.BasePn
         void CaseCompleted(object sender, EventArgs args);
         void CaseDeleted(object sender, EventArgs args);
         void NotificationNotFound(object sender, EventArgs args);
+        /// <summary>
+        /// This method will start the internal threads if any.
+        /// </summary>
+        /// <param name="sdkConnectionString">This is required if the plugin is to start the eForm SDK</param>
+        /// <returns></returns>
+        bool Start(string sdkConnectionString);
+        /// <summary>
+        /// This is to shut down the internal threads the plugin might have started
+        /// otherwise it should just return true.
+        /// </summary>
+        /// <param name="shutdownReallyFast">Optinally implemented so the plugin shuts down really fast</param>
+        /// <returns></returns>
+        bool Stop(bool shutdownReallyFast);
+        /// <summary>
+        /// If needed stops and start the internal threads of the plugin
+        /// </summary>
+        /// <param name="shutdownReallyFast">Optinally implemented so the plugin shuts down really fast</param>
+        /// <returns></returns>
+        bool Restart(bool shutdownReallyFast);
     }
 }
